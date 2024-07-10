c7324fbbd55ea6287442751a60a81e0e0bbef0a7
hacio22 <jyhspace@gmail.com>
2024-07-10 17:13:04 +0900

    feat: 읽기 파일

--- src/post/post.js
+++ src/post/post.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function Post() {
     const markdownFiles = [
-        { name: "dd", content: "avgfyvfga" },
-        { name: "dd", content: "abhhja" }
+        { name: "글1", content: "가보자" },
+        { name: "ㅇㅇd", content: "가보자고" }
     ];
     return (
         <div className="flex-1 py-8 px-6">
             <div className="container mx-auto grid gird-cols-1 md:grid-cols-3 gap-8">
                 <div className="col-span-3">
                     <h2 className="text-2xl font-bold mb-4">글</h2>
-                    <div className="gird grid-cols-2 md:grid-cols-2 gap-6">
+                    <div className="grid grid-cols-2 md:grid-cols-2 gap-6">
                         {markdownFiles.map((file, index) => (
                             <Link key={index} to={`/blog/${file.name}`}
                                 className="rounded-lg overflow-hidden shadow-md">
